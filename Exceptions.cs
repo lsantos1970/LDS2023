8c487dcfa25b463e8f600491a0914e5ce267cad9
JoanaDev <joana.pedroso15@gmail.com>
2023-04-19 17:23:03 +0100

    adicionar comentários na classe da excepções e no modelo

--- Exceptions.cs
+++ Exceptions.cs
@@ -6,23 +6,22 @@ using System.Threading.Tasks;
 
 namespace AnaliseImagens
 {
-    //Definição das diferentes excepções que esta classe pode lançar
-    class NoCommandFound : Exception
-    {
-        public NoCommandFound() : base("Deve introduzir um comando.") { }
-    }
+    //Definição das diferentes excepções que podem ser lançadas
 
 
+    //Quando comando não é válido
     class CommandNotValid : Exception
     {
         public CommandNotValid(string command) : base($"Comando {command} não é válido.") { }
     }
 
+    //Quando não existe nenhuma imagem no path indicado ou este não é válido
     class InvalidPath : Exception
     {
         public InvalidPath(string path) : base($"Não foi encontrada nenhuma imagem em {path}.") { }
     }
 
+    //Quando não foi possível executar a operação com sucesso
     class OperationError : Exception
     {
         public OperationError(string operation) : base($"Não foi posível executar a operação {operation} com sucesso.") { }
--- Model.cs
+++ Model.cs
@@ -6,6 +6,7 @@ using System.IO;
 namespace AnaliseImagens
 {
 
+    //Definição do objecto ColorPercentages
     public class ColorPercentages
     {
         public double RedPercentage { get; set; }
@@ -21,6 +22,8 @@ namespace AnaliseImagens
       
         public delegate void CommandExecutor(string commandReceived);
         public delegate void ResultsHandler(object sender, ResultsEventArgs e);
+
+        //Declaração de evento
         public event ResultsHandler OnResultsAvailable;
 
         private Dictionary<string, CommandExecutor> commandExecutors;
@@ -39,6 +42,15 @@ namespace AnaliseImagens
         }
 
         /*
+         * Implementação do método que lança o evento 'OnResultsAvailable'
+        */
+        protected virtual void RaiseResultsAvailable(ColorPercentages results)
+        {
+            OnResultsAvailable?.Invoke(this, new ResultsEventArgs(results));
+        }
+
+
+        /*
          * Retorna a lista de comandos disponíveis  
         */
         public List<string> ListarComandos() 
@@ -46,14 +58,20 @@ namespace AnaliseImagens
             return listCmds;
         }
 
-         protected virtual void RaiseResultsAvailable(ColorPercentages results)
-        {
-            OnResultsAvailable?.Invoke(this, new ResultsEventArgs(results));
-        }
 
-
+        /*
+         * Executa comando introduzido pelo utilizador. Se comando for executado com sucesso, o evento 'OnResultsAvailable' é lançado
+         */
         public void ExecutarComando(string commandReceived)
         {
+            /*
+             * TO DO - O comando possui na verdade o comando em si e o path da imagem. É necessário um método que separe ambos, valide 
+             * e lance uma excepção consoante o tipo de erro, por exemplo:
+             *  - Se comando inválido lança a excepção CommandNotValid
+             *  - Se imagem inválid lança a excepção InvalidPath
+             *  - Se operação não foi executada com sucesso, lança a excepção OperationError
+             */
+
             if (commandExecutors.TryGetValue(commandReceived, out CommandExecutor executor))
             {
                 executor(commandReceived);
